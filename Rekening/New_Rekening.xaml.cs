9a1d60f88584610d289ad6f666e03b2c0baff30e
samsu007 <itdev.nuansa@gmail.com>
2026-07-28 07:35:02 +0800

    Fix

---
+++ Rekening/New_Rekening.xaml.cs
@@ -0,0 +1,17 @@
+using CommunityToolkit.Maui.Alerts;
+
+namespace FinanceApp.Rekening;
+
+
+public partial class New_Rekening : ContentPage
+{
+	public New_Rekening()
+	{
+		InitializeComponent();
+	}
+
+    private void BSimpan_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
