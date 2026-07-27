1717b1cc7816f3d96edae9c3a69c62fe00ccfeaf
samsu007 <itdev.nuansa@gmail.com>
2026-07-27 17:02:52 +0800

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace FinanceApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
---
+++ Rekening/List-Akun.xaml.cs
@@ -0,0 +1,9 @@
+namespace FinanceApp.Rekening;
+
+public partial class List_Akun : ContentPage
+{
+	public List_Akun()
+	{
+		InitializeComponent();
+	}
+}
