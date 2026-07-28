043b7d82c7d06490f1059df5410f6c57eab2aa0d
samsu007 <itdev.nuansa@gmail.com>
2026-07-28 13:31:06 +0800

    Fix UI

--- Kategori/New_Kategori.xaml.cs
+++ Kategori/New_Kategori.xaml.cs
@@ -11,4 +11,14 @@ public partial class New_Kategori : ContentPage
     {
 
     }
+
+    private void BPemasukan_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void BPengeluaran_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
