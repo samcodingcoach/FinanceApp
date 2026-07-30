64358dd035990a60f16517e63ab21d1b2fd92905
samsu007 <itdev.nuansa@gmail.com>
2026-07-30 17:06:23 +0800

    Fix

---
+++ Budget/List_Budget.xaml.cs
@@ -0,0 +1,14 @@
+namespace FinanceApp.Budget;
+
+public partial class List_Budget : ContentPage
+{
+	public List_Budget()
+	{
+		InitializeComponent();
+	}
+
+    private void T_Search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+}
---
+++ Budget/New_Budget.xaml.cs
@@ -0,0 +1,9 @@
+namespace FinanceApp.Budget;
+
+public partial class New_Budget : ContentPage
+{
+	public New_Budget()
+	{
+		InitializeComponent();
+	}
+}
